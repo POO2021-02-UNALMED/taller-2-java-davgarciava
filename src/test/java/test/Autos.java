46e5b2ac553e7ca7c5360002d72c47c2440fc35c
DovydasHartz <davgarciava@unal.edu.co>
2021-10-15 21:30:30 -0500

    Entrega actividad

---
+++ src/test/java/test/Autos.java
@@ -0,0 +1,36 @@
+package test;
+
+public class Autos {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int c = 0;
+		for(int i = 0;i < asientos.length;i++) {
+			if(asientos[i]!=null) {
+				c++;
+			}
+		}
+		return c;
+	}
+	
+	String verificarIntegridad() {
+		if(registro==motor.registro) {
+			for(int i = 0;i < asientos.length;i++) {
+				if(asientos[i]!=null) {
+					if(registro!=asientos[i].registro) {
+						return "Las piezas no son originales";
+					}
+				}
+			}
+		} else {
+			return "Las piezas no son originales";
+		}
+		return "Auto original";
+	}
+}
